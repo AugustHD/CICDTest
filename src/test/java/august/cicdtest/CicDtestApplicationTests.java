e88c3da381684c2bb64113bf7ce86ff7001a04b2
augu0577 <august.hawk@gmail.com>
2022-09-09 11:07:05 +0200

    Initial commit

---
+++ src/test/java/august/cicdtest/CicDtestApplicationTests.java
@@ -0,0 +1,13 @@
+package august.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicDtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
